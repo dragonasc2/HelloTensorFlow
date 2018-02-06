01d38f4be7fb55e65902fe8ff885d1d9ee078b23
dragonasc2 <dragon_sc2@hotmail.com>
2018-02-06 17:22:01 +0800

    finished SVM

--- CS231n/assignment1/cs231n/classifiers/linear_classifier.py
+++ CS231n/assignment1/cs231n/classifiers/linear_classifier.py
@@ -38,6 +38,7 @@ class LinearClassifier(object):
 
     # Run stochastic gradient descent to optimize W
     loss_history = []
+    full_idx = np.arange(num_train)
     for it in xrange(num_iters):
       X_batch = None
       y_batch = None
@@ -53,7 +54,9 @@ class LinearClassifier(object):
       # Hint: Use np.random.choice to generate indices. Sampling with         #
       # replacement is faster than sampling without replacement.              #
       #########################################################################
-      pass
+      sampling_idx = np.random.choice(full_idx, batch_size, replace=False)
+      X_batch = X[sampling_idx]
+      y_batch = y[sampling_idx]
       #########################################################################
       #                       END OF YOUR CODE                                #
       #########################################################################
@@ -67,7 +70,7 @@ class LinearClassifier(object):
       # TODO:                                                                 #
       # Update the weights using the gradient and the learning rate.          #
       #########################################################################
-      pass
+      self.W -= learning_rate * grad
       #########################################################################
       #                       END OF YOUR CODE                                #
       #########################################################################
@@ -96,7 +99,8 @@ class LinearClassifier(object):
     # TODO:                                                                   #
     # Implement this method. Store the predicted labels in y_pred.            #
     ###########################################################################
-    pass
+    Y = X.dot(self.W)
+    y_pred = np.argmax(Y, axis=1)
     ###########################################################################
     #                           END OF YOUR CODE                              #
     ###########################################################################
--- CS231n/assignment1/cs231n/classifiers/linear_svm.py
+++ CS231n/assignment1/cs231n/classifiers/linear_svm.py
@@ -29,12 +29,17 @@ def svm_loss_naive(W, X, y, reg):
   for i in xrange(num_train):
     scores = X[i].dot(W)
     correct_class_score = scores[y[i]]
+    dLdY = np.zeros((1, num_classes), dtype=np.float32)
     for j in xrange(num_classes):
       if j == y[i]:
         continue
       margin = scores[j] - correct_class_score + 1 # note delta = 1
       if margin > 0:
         loss += margin
+        dLdY[0, j] += 1
+        dLdY[0, y[i]] -= 1
+    delta = X[i:i+1, :].T.dot(dLdY)
+    dW += delta
 
   # Right now the loss is a sum over all training examples, but we want it
   # to be an average instead so we divide by num_train.
@@ -42,6 +47,8 @@ def svm_loss_naive(W, X, y, reg):
 
   # Add regularization to the loss.
   loss += reg * np.sum(W * W)
+  dW /= num_train
+  dW += 2*W
 
   #############################################################################
   # TODO:                                                                     #
@@ -70,7 +77,12 @@ def svm_loss_vectorized(W, X, y, reg):
   # Implement a vectorized version of the structured SVM loss, storing the    #
   # result in loss.                                                           #
   #############################################################################
-  pass
+  
+  Y = np.dot(X, W)
+  idx = np.arange(X.shape[0])
+  margin = Y - np.reshape(Y[idx, y], (X.shape[0], 1)) + 1
+  margin[idx, y] = 0
+  loss = np.sum(margin[margin>0]) / X.shape[0] + reg * np.sum(np.multiply(W, W))
   #############################################################################
   #                             END OF YOUR CODE                              #
   #############################################################################
@@ -85,6 +97,11 @@ def svm_loss_vectorized(W, X, y, reg):
   # to reuse some of the intermediate values that you used to compute the     #
   # loss.                                                                     #
   #############################################################################
+  dLdY = np.zeros((X.shape[0], W.shape[1]))
+  dLdY[margin>0] = 1
+  dLdY[idx, y] = - np.sum(margin>0, axis=1)
+  dW = X.T.dot(dLdY) / X.shape[0] + 2 * W
+
   pass
   #############################################################################
   #                             END OF YOUR CODE                              #
